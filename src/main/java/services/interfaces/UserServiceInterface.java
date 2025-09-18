60668ddeca6dc923a3d573aee51f98120fd10cc0
Valentina Velasquez Velez <valentina.velasquez@globant.com>
2025-09-18 14:44:41 -0500

    Updates

--- src/main/java/services/interfaces/UserServiceInterface.java
+++ src/main/java/services/interfaces/UserServiceInterface.java
@@ -1,17 +1,4 @@
 package services.interfaces;
 
-import model.domain.User;
-import java.util.List;
-import java.util.Optional;
-
-public class UserServiceInterface {
-
-    Optional<User> login(String email, String password);
-    void registrar(User u);
-    void actualizar(User u);
-    void eliminar(int id);
-    Optional<User> buscarPorEmail(String email);
-    Optional<User> buscarPorId(int id);
-    List<String> listarConPrestamosActivos();
-
+public interface UserServiceInterface {
 }
